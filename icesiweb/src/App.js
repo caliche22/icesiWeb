e3df5cb242df786c6283f5f5397da17c61b1faa1
Carlos Arboleda <caliche.a2@gmail.com>
2020-03-06 17:41:19 -0500

    idea de registro mas el boton de acceso con google , lo acomodas el front y las demas maricadas

--- icesiweb/src/App.js
+++ icesiweb/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './login';
+import { UserForm } from './components/UserForm';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Login/>
+        <UserForm/>
       </header>
     </div>
   );
